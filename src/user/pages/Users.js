93a8268cd810b1548bcc7e88fb461976d03e4eae
saptarshi1729 <saptarshim@iitbhilai.ac.in>
2024-12-29 23:58:28 +0530

    Initial commit: Upload VoyageHub frontend

---
+++ src/user/pages/Users.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import UserList from '../components/UserList';
+
+const Users = () => {
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Sap',
+      image: 'https://t3.ftcdn.net/jpg/02/36/99/22/360_F_236992283_sNOxCVQeFLd5pdqaKGh8DRGMZy7P4XKm.jpg',
+      places: 2
+    }
+  ];
+  return <UserList items={USERS} />;
+};
+
+export default Users;
