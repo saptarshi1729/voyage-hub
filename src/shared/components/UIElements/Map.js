3a52c347d09b44bfe2cfc05eb591e55ae15f5a99
saptarshi1729 <saptarshim@iitbhilai.ac.in>
2024-12-31 15:28:12 +0530

    Add functionality to display map

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,40 @@
+import React, { useRef, useEffect } from 'react';
+import MapLibrary from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import { fromLonLat } from 'ol/proj';
+
+import './Map.css';
+
+const Map = props => {
+  const mapRef = useRef();
+
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    new MapLibrary({
+      target: mapRef.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: fromLonLat([center.lng, center.lat]),
+        zoom: zoom,
+      }),
+    });
+  }, [center, zoom]);
+
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+      id="map"
+    ></div>
+  );
+};
+
+export default Map;
