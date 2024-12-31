e17e566cf2c5a153925a1e9e827c6bd4faf34732
saptarshi1729 <saptarshim@iitbhilai.ac.in>
2025-01-01 02:08:28 +0530

    Added custom hook and implemented UpdatePlace

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,58 +1,38 @@
-import React, { useCallback, useReducer } from "react";
+import React from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
-import "./NewPlace.css";
-
-const formReducer = (state, action) => {
-  switch(action.type) {
-    case 'INPUT_CHANGE':
-      let formIsValid = true;
-      for (const inputId in state.inputs) {
-        if (inputId === action.inputId) {
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: {value: action.value, isValid: action.isValid},
-        },
-        isValid: formIsValid,
-      }
-    default:
-      return state;
-  }
-};
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import { useForm } from "../../shared/hooks/form-hook";
+import "./PlaceForm.css";
 
 const NewPlace = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
+  const [formState, inputHandler] = useForm(
+    {
       title: {
-        value: '',
+        value: "",
         isValid: false,
       },
       description: {
-        value: '',
+        value: "",
         isValid: false,
-      }
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
     },
-    isValid: false
-  });
+    false
+  );
 
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({type: 'INPUT_CHANGE', value: value, isValid: isValid, inputId: id});
-  }, []);
-
-  const placeSubmitHandler = event => {
+  const placeSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(formState.inputs);  // send this to backend later.
+    console.log(formState.inputs); // send this to backend later.
   };
-  
+
   return (
     <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
@@ -80,7 +60,9 @@ const NewPlace = () => {
         errorText="Please enter a valid address"
         onInput={inputHandler}
       />
-      <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
     </form>
   );
 };
