43cf9ef9b1aab670f7300df82c09574e4645024f
saptarshi1729 <saptarshim@iitbhilai.ac.in>
2025-03-14 16:18:18 +0530

    Add auth context and listen to context

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,36 @@
-import React from "react";
-import { NavLink } from 'react-router-dom';
+import React, { createContext, useContext } from "react";
+import { NavLink } from "react-router-dom";
 
-import './NavLinks.css'
+import { AuthContext } from "../../context/auth-context";
+import "./NavLinks.css";
 
-const NavLinks = props => {
-    return <ul className="nav-links">
+const NavLinks = (props) => {
+  const auth = useContext(AuthContext);
+
+  return (
+    <ul className="nav-links">
+      <li>
+        <NavLink to="/" exact>
+          ALL USERS
+        </NavLink>
+      </li>
+      {auth.isLoggedIn && (
         <li>
-            <NavLink to="/" exact>ALL USERS</NavLink>
+          <NavLink to="/u1/places">MY PLACES</NavLink>
         </li>
+      )}
+      {auth.isLoggedIn && (
         <li>
-            <NavLink to="/u1/places">MY PLACES</NavLink>
+          <NavLink to="/places/new">ADD PLACES</NavLink>
         </li>
+      )}
+      {!auth.isLoggedIn && (
         <li>
-            <NavLink to="/places/new">ADD PLACES</NavLink>
+          <NavLink to="/auth">AUTHENTICATE</NavLink>
         </li>
-        <li>
-            <NavLink to="/auth">AUTHENTICATE</NavLink>
-        </li>
+      )}
     </ul>
+  );
 };
 
 export default NavLinks;
